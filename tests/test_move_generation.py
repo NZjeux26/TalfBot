37cba21c0a76dad5deba6b671c016f9b7aefa20c
NZjeux26 <91103932+NZjeux26@users.noreply.github.com>
2025-03-14 11:52:46 +1300

    create test function for capturing pieces against hostile sqaures

--- tests/test_move_generation.py
+++ tests/test_move_generation.py
@@ -90,6 +90,44 @@ def test_black_capture_white_TB(game, initial_layout):
     # Check if a Black piece was captured (count should decrease by one)
     print(f"Black pieces before: {white_pieces_before}, after: {white_pieces_after}")
     assert white_pieces_after == white_pieces_before - 1, "A Black piece should have been captured!"
+
+#This function tests capturing pieces against hostile squares for corners and an empty throne
+def test_hostile_corner_squares_black(game, initial_layout):
+    
+    #setup corner test positions for corner test
+    initial_layout[game.BLACK, 0, 1] = 1 #Black piece in B11
+    initial_layout[game.WHITE, 1, 2] = 1 #White piece in C10
+    
+    #setup for capturing against the throne
+    initial_layout[game.WHITE, 4, 5] = 1 #White piece in F7
+    initial_layout[game.BLACK, 2, 5] = 1 #Black in F9
+    
+    #Print initial board layout
+    print("Initial Board Layout:")
+    print_board(initial_layout)
+    
+    # Count all pieces before the move. We use both Black and white since after each play the colour in play switches in make_move
+    pieces_before = np.sum(initial_layout[game.WHITE]) + np.sum(initial_layout[game.BLACK])
+    
+    #move to caoture a piece that is against the emtpy throne
+    move = ((2, 5), (3, 5))  # move black from F7 to C8
+    new_state = game.make_move(initial_layout, move)
+    
+    #Make the move to capture the black piece in the corner
+    move = ((1, 2), (0, 2))  # move white from c10 -> C11
+    new_state = game.make_move(new_state, move) #We make the move from new_state since a piece has already been moved
+    
+    #Print resulting board layout
+    print("Resulting Board Layout:")
+    print_board(new_state)
+    
+    # Count pieces after the move, if the pieces have been taken they would have been removed from the baord and missing in the array
+    pieces_after = np.sum(new_state[game.WHITE]) + np.sum(new_state[game.BLACK])
+
+    # Check if a piece was captured (count should decrease by one)
+    print(f"Game pieces before: {pieces_before}, after: {pieces_after}")
+    assert pieces_after == pieces_before - 2, "A game piece should have been captured!"
+    
     
 if __name__ == '__main__':
     pytest.main()
